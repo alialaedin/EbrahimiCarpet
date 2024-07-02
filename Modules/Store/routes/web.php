271615396fd87442362cd50e129908c749d93e6a
alialaedin <aliallaedin1383@gmail.com>
2024-07-02 09:20:22 +0330

    store

--- Modules/Store/routes/web.php
+++ Modules/Store/routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\Store\Http\Controllers\Admin\StoreController;
 
 Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function () {
-	Route::post('/stores/increase-decrease', [StoreController::class, 'increase_decrease'])->name('stores.increase-decrease');
+	Route::put('/stores/increase-decrease', [StoreController::class, 'increase_decrease'])->name('stores.increase-decrease');
 	Route::resource('/stores', StoreController::class)->only('index', 'show');
 });
