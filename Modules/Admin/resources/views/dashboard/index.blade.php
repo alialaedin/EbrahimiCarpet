136a5707d21621aa0e8b762e70bca411f087b3f4
ali alaeddin <alialaedin1383@gmail.co>
2024-08-13 11:36:02 +0330

    edit margin in dashboard

--- Modules/Admin/resources/views/dashboard/index.blade.php
+++ Modules/Admin/resources/views/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.master')
 @section('content')
-  <div class="page-header">
+  <div class="page-header" style="margin-bottom: 1.5rem;">
     <ol class="breadcrumb align-items-center">
       <li class="breadcrumb-item active">
         <i class="fe fe-home ml-1"></i> داشبورد
