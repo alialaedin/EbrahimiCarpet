f96f0593e173587dbe8de90eee96f40dddba07f0
ali alaeddin <alialaedin1383@gmail.com>
2024-10-07 10:34:52 +0330

    .

--- Modules/Payment/resources/views/cashes.blade.php
+++ Modules/Payment/resources/views/cashes.blade.php
@@ -61,7 +61,14 @@
             </div>
           </div>
         </div>
-        git 
+        <div class="row">
+          <div class="col-xl-9 col-lg-8 col-md-6 col-12">
+            <button class="btn btn-primary btn-block" type="submit">جستجو <i class="fa fa-search"></i></button>
+          </div>
+          <div class="col-xl-3 col-lg-4 col-md-6 col-12">
+            <a href="{{ route("admin.payments.cashes") }}" class="btn btn-danger btn-block">حذف همه فیلتر ها <i class="fa fa-close"></i></a>
+          </div>
+        </div>
       </form>
     </x-slot>
   </x-core::card>
