403c85acdf746d86e2205c89835b280d2d768079
alialaedin <alialaedin1234@gmail.com>
2024-12-25 12:16:45 +0330

    .

--- Modules/Purchase/resources/views/purchase/edit.blade.php
+++ Modules/Purchase/resources/views/purchase/edit.blade.php
@@ -54,6 +54,13 @@
         </div>
       </div>
       <div class="row">
+        <div class="col-12 text-center mb-5 bg-black-8 text-white-80 py-3 rounded" >
+          <span class="fs-16">جمع مبلغ کل فاکتور : </span>
+          <span class="font-weight-bold fs-16" id="totalPrice"></span>
+          <span class="font-weight-bold fs-16">ریال</span>
+        </div>
+      </div>
+      <div class="row">
         <div class="col">
           <div class="text-center">
             <button class="btn btn-warning" type="submit">بروزرسانی</button>
@@ -68,4 +75,24 @@
 
 @section('scripts')
   <x-core::date-input-script textInputId="purchased_date_show" dateInputId="purchased_date"/>
+
+  <script>
+
+    const discountInput = $('#discount');
+    const totalItemsAmount = parseInt(@json($purchase->total_items_amount));
+
+    function calculateTotalAmount() {
+      let discountAmount = discountInput.val() != null && discountInput.val().length > 0 ? parseInt(discountInput.val().replace(/,/g, '')) : 0;
+      let finalPrice = (totalItemsAmount - discountAmount).toLocaleString();
+      $('#totalPrice').text(finalPrice);
+    }
+
+    $(document).ready(() => {
+      calculateTotalAmount();
+      discountInput.on('input', calculateTotalAmount);
+      costOfSewingInput.on('input', calculateTotalAmount);
+    });
+
+  </script>
+
 @endsection
