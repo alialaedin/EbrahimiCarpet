eda5f03a489b46a390864879aa9f7a054de7505a
alialaedin <alialaedin1234@gmail.com>
2025-01-23 12:18:40 +0330

    .

--- Modules/Core/app/Rules/IranMobile.php
+++ Modules/Core/app/Rules/IranMobile.php
@@ -12,7 +12,7 @@ class IranMobile implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (!preg_match('/^09(1[0-9]|9[0-2]|2[0-2]|0[1-5]|41|3[0,3,5-9])\d{7}$/', $value)) {
+        if (!preg_match('/^09(1[0-9]|9[0-4]|2[0-2]|0[1-5]|41|3[0,3,5-9])\d{7}$/', $value)) {
             $fail('شماره موبایل وارد شده نامعتبر است!');
         }
     }
